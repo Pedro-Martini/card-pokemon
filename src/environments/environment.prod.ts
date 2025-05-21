89de4b961c6d3b188225328858cd1ad02bceb6a8
Pedro-Martini <pederiquemartine@gmail.com>
2025-05-21 13:51:05 -0300

    criação config proxy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://pokeapi.co/api/v2/',
-  corsProxy : 'https://cors-anywhere.herokuapp.com/'
+  corsProxy : 'https://cors-anywhere.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://pokeapi.co/api/v2/',
-  corsProxy : 'https://cors-anywhere.herokuapp.com/'
+  corsProxy : 'https://cors-anywhere.com/'
 };
 
 
